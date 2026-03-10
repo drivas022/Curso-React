52fbcd4dde00322ece99a0c72b681fda60a4e400
Diego Rivas <drivas022@yahoo.com>
2026-03-10 12:44:07 -0600

    feat: Interfaces de Typescript

--- react/01-reforzamiento/src/bases/03-object-literal.ts
+++ react/01-reforzamiento/src/bases/03-object-literal.ts
@@ -1,20 +1,36 @@
-const ironman = {
+interface Person {
+    firstName: string;
+    lastName: string;
+    age: number;
+
+    // Con este signo de interrogacion, se hace que la propiedad sea opcional, es decir, no es necesario que el objeto tenga esa propiedad
+    address: Address;
+}
+interface Address {
+    zipCode: number;
+    city: string;
+}
+
+const ironman: Person = {
     firstName: 'Tony',
     lastName: 'Stark',
     age: 45,
     address: {
-        zipCode: 'ABC123',
+        zipCode: 1321412,
         city: 'New York',
-        country: 'USA'
     }
+
 }
 
-const spiderman = structuredClone(ironman); // toma todas las properties que estan en ironman y las asigna a spiderman, es decir, hace una copia de ironman
 
-spiderman.firstName = 'Peter';
-spiderman.lastName = 'Parker';
-spiderman.age = 18;
-spiderman.address.city = 'San Jose'; // copia
+console.log(ironman)
+
+// const spiderman = structuredClone(ironman); // toma todas las properties que estan en ironman y las asigna a spiderman, es decir, hace una copia de ironman
+
+// spiderman.firstName = 'Peter';
+// spiderman.lastName = 'Parker';
+// spiderman.age = 18;
+// spiderman.address.city = 'San Jose'; // copia
 
 
-console.log(ironman, spiderman)
+// console.log(ironman, spiderman)
